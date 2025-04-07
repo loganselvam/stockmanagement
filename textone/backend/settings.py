846eb0ed4d79711972b8c68b0aaf909ce3c1d90c
loganselvam <loganathanselvam2000@gmail.com>
2025-04-07 10:11:59 +0530

    databsed fixed

--- textone/backend/settings.py
+++ textone/backend/settings.py
@@ -97,9 +97,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbname_jkmq',
+        'USER': 'salesman',
+        'PASSWORD': 'YYs1GSyN4r3EXx5ujLClsJke0uiZsXF3',
+        'HOST': 'dpg-cvpc6uvgi27c73b3i0e0-a.oregon-postgres.render.com',
+        'PORT': '5432',
+    }
 }
+
 
 
 # Password validation
